c13da7e054d84857abff59d22809d7c86870d083
Tio Yae <1246336370@qq.com>
2022-05-11 08:01:06 +0800

    修改了抽卡数量的概率

--- src/main/java/org/TioYae/mirai/DailyDrawCard.java
+++ src/main/java/org/TioYae/mirai/DailyDrawCard.java
@@ -16,12 +16,16 @@ public class DailyDrawCard {
     public List<String> draw() {
         HashSet<Integer> draw = new HashSet<>();
         Random random = new Random();
-        int n = random.nextInt(4); // 数量区间为[0, 3]
+        int n = random.nextInt(100); // 数量区间为[0, 3]
+        if (n < 10) n = 0; // 抽不到的概率为10%
+        else if (n < 50) n = 1; // 抽到一个的概率为40%
+        else if (n < 90) n = 2; // 抽到两个的概率为40%
+        else n = 3; // 抽到三个的概率为10%
         if (n == 0) return null;
         List<String> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int t = random.nextInt(roleData.length());
-            while(draw.contains(t)){
+            while (draw.contains(t)) {
                 t = random.nextInt(roleData.length());
             }
             draw.add(t);
